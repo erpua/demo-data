6e3ae59e5628d687c5b28da63002cc1805887fc1
intn <ievgen.tovtin@gft.com>
2022-08-31 14:03:49 -0500

    added new backend for react academy

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const userData = require('./demodata.json');
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Demo data for React Academy');
+});
+
+app.get('/users', (req, res) => {
+  res.send(userData);
+});
+
+app.listen(port, () => {
+  console.log(`App is listening too port ${port}`);
+});
